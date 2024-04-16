005819fb57fa119454348eaad58dede62e71c93c
GROGUEUR <157125419+GROGUEUR@users.noreply.github.com>
2024-04-16 11:30:25 +0200

    Add files via upload

---
+++ lib/fbibliotheque.h
@@ -0,0 +1,10 @@
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include <SDL2/SDL_ttf.h>
+#include <SDL2/SDL_mixer.h>
+#include <stdio.h>
+
+#define WINDOW_WIDTH 1360
+#define WINDOW_HEIGHT 780
+
+int charger_map_biblio(char * CARTE_LIGNE,char * CARTE_TXT,char * nom_carte, int tab[125][250]);
