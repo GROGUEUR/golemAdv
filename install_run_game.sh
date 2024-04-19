7e44436d07eaf8c47ee62ef092774caa91be84c7
Ge0mega <157126423+Ge0mega@users.noreply.github.com>
2024-04-19 18:46:05 +0200

    Update install_run_game.sh

--- install_run_game.sh
+++ install_run_game.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-if [ ! -d "Golem_Adventures1.9.6officiel" ];then
-    mkdir Golem_Adventures1.9.6officiel
-    cd Golem_Adventures1.9.6officiel
+if [ ! -d "GolemAdventure" ];then
+    mkdir GolemAdventure
+    cd GolemAdventure
 
-    wget -P . https://raw.githubusercontent.com/GROGUEUR/golemAdv/main/Golem_Adventures1.9.6officiel.tar.gz
+    wget -P . https://raw.githubusercontent.com/GROGUEUR/golemAdv/main/GolemAdventures.tar.gz
 
     # Dézipper l'archive
-    tar -xvzf Golem_Adventures1.9.6officiel.tar.gz
+    tar -xvzf GolemAdventure.tar.gz
 
-    rm -f Golem_Adventures1.9.6officiel.tar.gz
+    rm -f GolemAdventure.tar.gz
 else
-    cd Golem_Adventures1.9.6officiel
+    cd GolemAdventure
 fi
 
 # Exécuter make run
